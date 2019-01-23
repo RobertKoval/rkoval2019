e4382f634d8bae06a40b0f4f2f6c20aebb0108e7
RobertKoval <kovalrobert@me.com>
2019-01-23 20:33:03 +0200

    Ok

---
+++ rkoval2019/Classes/Article+CoreDataClass.swift
@@ -0,0 +1,15 @@
+//
+//  Article+CoreDataClass.swift
+//  Pods-rkoval2019_Example
+//
+//  Created by Robert on 23.01.19.
+//
+//
+
+import Foundation
+import CoreData
+
+@objc(Article)
+public class Article: NSManagedObject {
+
+}
